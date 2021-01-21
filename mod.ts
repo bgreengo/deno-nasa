ae84316dfa4702e510de3c44e2da6ac0bbe4b566
Blake Green <blakegreen@msn.com>
2021-01-21 07:23:36 -0800

    add more stuff

--- mod.ts
+++ mod.ts
@@ -1,14 +1,44 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, send } from "https://deno.land/x/oak/mod.ts";
+
+import api from "./api.ts";
 
 const app = new Application();
-const port = 8000;
+const PORT = 8000;
 
-app.use((ctx) => {
-    ctx.response.body = "Hello World";
-})
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now();
+  ctx.response.headers.set("X-Response-Time", `${delta}ms`);
+});
+
+app.use(api.routes());
+app.use(api.allowedMethods());
+
+app.use(async (ctx) => {
+  const filePath = ctx.request.url.pathname;
+  const fileWhitelist = [
+    "/index.html",
+    "/javascripts/script.js",
+    "/stylesheets/style.css",
+    "/images/favicon.png",
+  ];
+  // Make sure only approved files in whitelist are allowed
+  if (fileWhitelist.includes(filePath)) {
+    await send(ctx, filePath, {
+      root: `${Deno.cwd()}/public`,
+    });
+  }
+});
 
 if (import.meta.main) {
-    await app.listen({
-        port: port
-    });
+  await app.listen({
+    port: PORT,
+  });
 }
